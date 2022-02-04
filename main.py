d61aabde368a4e285982daeb11243d59464bb1cd
Basia <bao.zajac@gmail.com>
2022-02-04 21:09:08 +0100

    plik main.py: poprawienie wyswietlania 3 pierwszych inputow, skopiowanie wyniku programu do 24 razy

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
 kwota_pozyczki_poczatkowa = float(input("Podaj kwotę Twojej pożyczki: "))
+print(end="\n")
 oprocentowanie = float(input("Podaj oprocentowanie pożyczki (ilość %): "))
+print(end="\n")
 rata = float(input("Podaj wysokość miesięcznej raty kredytu: "))
+print(end="\n")
 
 inflacja = float(input())
 
-#TODO:  poprawić pobieranie inputów - jak zrobić, że część inputów jest podawana przez użytkownika, a część zaciągana z pliku .txt
-
-# z Excela: =(1 + ((inflacjaWDanymMiesiącu+3)/1200)) * kwotaPozostałaDoSpłacenia - 200
-pozostala_kwota_pozyczki = (1 + (inflacja + oprocentowanie) / 1200) * kwota_pozyczki_poczatkowa - 200
-roznica = kwota_pozyczki_poczatkowa - pozostala_kwota_pozyczki
 
 szablon = "miesiąc {}: Twoja pozostała kwota kredytu to {}zł, to o {}zł mniej niż w poprzednim miesiącu."
 nr_miesiaca = 0
 
+# z Excela: =(1 + ((inflacjaWDanymMiesiącu+3)/1200)) * kwotaPozostałaDoSpłacenia - 200
+pozostala_kwota_pozyczki = (1 + (inflacja + oprocentowanie) / 1200) * kwota_pozyczki_poczatkowa - 200
+roznica = kwota_pozyczki_poczatkowa - pozostala_kwota_pozyczki
+podsumowanie_miesiaca = szablon.format(nr_miesiaca + 1, (round(pozostala_kwota_pozyczki, 2)), (round(roznica, 2)))
+print(podsumowanie_miesiaca)
+
+nr_miesiaca += 1
+kwota_pozyczki_poczatkowa = pozostala_kwota_pozyczki
+pozostala_kwota_pozyczki = (1 + (inflacja + oprocentowanie) / 1200) * kwota_pozyczki_poczatkowa - 200
+roznica = kwota_pozyczki_poczatkowa - pozostala_kwota_pozyczki
 podsumowanie_miesiaca = szablon.format(nr_miesiaca + 1, (round(pozostala_kwota_pozyczki, 2)), (round(roznica, 2)))
 print(podsumowanie_miesiaca)
 
@@ -30,8 +38,142 @@ roznica = kwota_pozyczki_poczatkowa - pozostala_kwota_pozyczki
 podsumowanie_miesiaca = szablon.format(nr_miesiaca + 1, (round(pozostala_kwota_pozyczki, 2)), (round(roznica, 2)))
 print(podsumowanie_miesiaca)
 
-# print("Twoja pozostała kwota kredytu to ", round(kwota_pozyczki_koncowa, 2), "zł, ")
-# print("to o ", round(roznica, 2), "zł mniej niż w poprzednim miesiącu.")
+nr_miesiaca += 1
+kwota_pozyczki_poczatkowa = pozostala_kwota_pozyczki
+pozostala_kwota_pozyczki = (1 + (inflacja + oprocentowanie) / 1200) * kwota_pozyczki_poczatkowa - 200
+roznica = kwota_pozyczki_poczatkowa - pozostala_kwota_pozyczki
+podsumowanie_miesiaca = szablon.format(nr_miesiaca + 1, (round(pozostala_kwota_pozyczki, 2)), (round(roznica, 2)))
+print(podsumowanie_miesiaca)
 
-#TODO: napisać to dla 24 miesięcy
+nr_miesiaca += 1
+kwota_pozyczki_poczatkowa = pozostala_kwota_pozyczki
+pozostala_kwota_pozyczki = (1 + (inflacja + oprocentowanie) / 1200) * kwota_pozyczki_poczatkowa - 200
+roznica = kwota_pozyczki_poczatkowa - pozostala_kwota_pozyczki
+podsumowanie_miesiaca = szablon.format(nr_miesiaca + 1, (round(pozostala_kwota_pozyczki, 2)), (round(roznica, 2)))
+print(podsumowanie_miesiaca)
 
+nr_miesiaca += 1
+kwota_pozyczki_poczatkowa = pozostala_kwota_pozyczki
+pozostala_kwota_pozyczki = (1 + (inflacja + oprocentowanie) / 1200) * kwota_pozyczki_poczatkowa - 200
+roznica = kwota_pozyczki_poczatkowa - pozostala_kwota_pozyczki
+podsumowanie_miesiaca = szablon.format(nr_miesiaca + 1, (round(pozostala_kwota_pozyczki, 2)), (round(roznica, 2)))
+print(podsumowanie_miesiaca)
+
+nr_miesiaca += 1
+kwota_pozyczki_poczatkowa = pozostala_kwota_pozyczki
+pozostala_kwota_pozyczki = (1 + (inflacja + oprocentowanie) / 1200) * kwota_pozyczki_poczatkowa - 200
+roznica = kwota_pozyczki_poczatkowa - pozostala_kwota_pozyczki
+podsumowanie_miesiaca = szablon.format(nr_miesiaca + 1, (round(pozostala_kwota_pozyczki, 2)), (round(roznica, 2)))
+print(podsumowanie_miesiaca)
+
+nr_miesiaca += 1
+kwota_pozyczki_poczatkowa = pozostala_kwota_pozyczki
+pozostala_kwota_pozyczki = (1 + (inflacja + oprocentowanie) / 1200) * kwota_pozyczki_poczatkowa - 200
+roznica = kwota_pozyczki_poczatkowa - pozostala_kwota_pozyczki
+podsumowanie_miesiaca = szablon.format(nr_miesiaca + 1, (round(pozostala_kwota_pozyczki, 2)), (round(roznica, 2)))
+print(podsumowanie_miesiaca)
+
+nr_miesiaca += 1
+kwota_pozyczki_poczatkowa = pozostala_kwota_pozyczki
+pozostala_kwota_pozyczki = (1 + (inflacja + oprocentowanie) / 1200) * kwota_pozyczki_poczatkowa - 200
+roznica = kwota_pozyczki_poczatkowa - pozostala_kwota_pozyczki
+podsumowanie_miesiaca = szablon.format(nr_miesiaca + 1, (round(pozostala_kwota_pozyczki, 2)), (round(roznica, 2)))
+print(podsumowanie_miesiaca)
+
+nr_miesiaca += 1
+kwota_pozyczki_poczatkowa = pozostala_kwota_pozyczki
+pozostala_kwota_pozyczki = (1 + (inflacja + oprocentowanie) / 1200) * kwota_pozyczki_poczatkowa - 200
+roznica = kwota_pozyczki_poczatkowa - pozostala_kwota_pozyczki
+podsumowanie_miesiaca = szablon.format(nr_miesiaca + 1, (round(pozostala_kwota_pozyczki, 2)), (round(roznica, 2)))
+print(podsumowanie_miesiaca)
+
+nr_miesiaca += 1
+kwota_pozyczki_poczatkowa = pozostala_kwota_pozyczki
+pozostala_kwota_pozyczki = (1 + (inflacja + oprocentowanie) / 1200) * kwota_pozyczki_poczatkowa - 200
+roznica = kwota_pozyczki_poczatkowa - pozostala_kwota_pozyczki
+podsumowanie_miesiaca = szablon.format(nr_miesiaca + 1, (round(pozostala_kwota_pozyczki, 2)), (round(roznica, 2)))
+print(podsumowanie_miesiaca)
+
+nr_miesiaca += 1
+kwota_pozyczki_poczatkowa = pozostala_kwota_pozyczki
+pozostala_kwota_pozyczki = (1 + (inflacja + oprocentowanie) / 1200) * kwota_pozyczki_poczatkowa - 200
+roznica = kwota_pozyczki_poczatkowa - pozostala_kwota_pozyczki
+podsumowanie_miesiaca = szablon.format(nr_miesiaca + 1, (round(pozostala_kwota_pozyczki, 2)), (round(roznica, 2)))
+print(podsumowanie_miesiaca)
+
+nr_miesiaca += 1
+kwota_pozyczki_poczatkowa = pozostala_kwota_pozyczki
+pozostala_kwota_pozyczki = (1 + (inflacja + oprocentowanie) / 1200) * kwota_pozyczki_poczatkowa - 200
+roznica = kwota_pozyczki_poczatkowa - pozostala_kwota_pozyczki
+podsumowanie_miesiaca = szablon.format(nr_miesiaca + 1, (round(pozostala_kwota_pozyczki, 2)), (round(roznica, 2)))
+print(podsumowanie_miesiaca)
+
+nr_miesiaca += 1
+kwota_pozyczki_poczatkowa = pozostala_kwota_pozyczki
+pozostala_kwota_pozyczki = (1 + (inflacja + oprocentowanie) / 1200) * kwota_pozyczki_poczatkowa - 200
+roznica = kwota_pozyczki_poczatkowa - pozostala_kwota_pozyczki
+podsumowanie_miesiaca = szablon.format(nr_miesiaca + 1, (round(pozostala_kwota_pozyczki, 2)), (round(roznica, 2)))
+print(podsumowanie_miesiaca)
+
+nr_miesiaca += 1
+kwota_pozyczki_poczatkowa = pozostala_kwota_pozyczki
+pozostala_kwota_pozyczki = (1 + (inflacja + oprocentowanie) / 1200) * kwota_pozyczki_poczatkowa - 200
+roznica = kwota_pozyczki_poczatkowa - pozostala_kwota_pozyczki
+podsumowanie_miesiaca = szablon.format(nr_miesiaca + 1, (round(pozostala_kwota_pozyczki, 2)), (round(roznica, 2)))
+print(podsumowanie_miesiaca)
+
+nr_miesiaca += 1
+kwota_pozyczki_poczatkowa = pozostala_kwota_pozyczki
+pozostala_kwota_pozyczki = (1 + (inflacja + oprocentowanie) / 1200) * kwota_pozyczki_poczatkowa - 200
+roznica = kwota_pozyczki_poczatkowa - pozostala_kwota_pozyczki
+podsumowanie_miesiaca = szablon.format(nr_miesiaca + 1, (round(pozostala_kwota_pozyczki, 2)), (round(roznica, 2)))
+print(podsumowanie_miesiaca)
+
+nr_miesiaca += 1
+kwota_pozyczki_poczatkowa = pozostala_kwota_pozyczki
+pozostala_kwota_pozyczki = (1 + (inflacja + oprocentowanie) / 1200) * kwota_pozyczki_poczatkowa - 200
+roznica = kwota_pozyczki_poczatkowa - pozostala_kwota_pozyczki
+podsumowanie_miesiaca = szablon.format(nr_miesiaca + 1, (round(pozostala_kwota_pozyczki, 2)), (round(roznica, 2)))
+print(podsumowanie_miesiaca)
+
+nr_miesiaca += 1
+kwota_pozyczki_poczatkowa = pozostala_kwota_pozyczki
+pozostala_kwota_pozyczki = (1 + (inflacja + oprocentowanie) / 1200) * kwota_pozyczki_poczatkowa - 200
+roznica = kwota_pozyczki_poczatkowa - pozostala_kwota_pozyczki
+podsumowanie_miesiaca = szablon.format(nr_miesiaca + 1, (round(pozostala_kwota_pozyczki, 2)), (round(roznica, 2)))
+print(podsumowanie_miesiaca)
+
+nr_miesiaca += 1
+kwota_pozyczki_poczatkowa = pozostala_kwota_pozyczki
+pozostala_kwota_pozyczki = (1 + (inflacja + oprocentowanie) / 1200) * kwota_pozyczki_poczatkowa - 200
+roznica = kwota_pozyczki_poczatkowa - pozostala_kwota_pozyczki
+podsumowanie_miesiaca = szablon.format(nr_miesiaca + 1, (round(pozostala_kwota_pozyczki, 2)), (round(roznica, 2)))
+print(podsumowanie_miesiaca)
+
+nr_miesiaca += 1
+kwota_pozyczki_poczatkowa = pozostala_kwota_pozyczki
+pozostala_kwota_pozyczki = (1 + (inflacja + oprocentowanie) / 1200) * kwota_pozyczki_poczatkowa - 200
+roznica = kwota_pozyczki_poczatkowa - pozostala_kwota_pozyczki
+podsumowanie_miesiaca = szablon.format(nr_miesiaca + 1, (round(pozostala_kwota_pozyczki, 2)), (round(roznica, 2)))
+print(podsumowanie_miesiaca)
+
+nr_miesiaca += 1
+kwota_pozyczki_poczatkowa = pozostala_kwota_pozyczki
+pozostala_kwota_pozyczki = (1 + (inflacja + oprocentowanie) / 1200) * kwota_pozyczki_poczatkowa - 200
+roznica = kwota_pozyczki_poczatkowa - pozostala_kwota_pozyczki
+podsumowanie_miesiaca = szablon.format(nr_miesiaca + 1, (round(pozostala_kwota_pozyczki, 2)), (round(roznica, 2)))
+print(podsumowanie_miesiaca)
+
+nr_miesiaca += 1
+kwota_pozyczki_poczatkowa = pozostala_kwota_pozyczki
+pozostala_kwota_pozyczki = (1 + (inflacja + oprocentowanie) / 1200) * kwota_pozyczki_poczatkowa - 200
+roznica = kwota_pozyczki_poczatkowa - pozostala_kwota_pozyczki
+podsumowanie_miesiaca = szablon.format(nr_miesiaca + 1, (round(pozostala_kwota_pozyczki, 2)), (round(roznica, 2)))
+print(podsumowanie_miesiaca)
+
+nr_miesiaca += 1
+kwota_pozyczki_poczatkowa = pozostala_kwota_pozyczki
+pozostala_kwota_pozyczki = (1 + (inflacja + oprocentowanie) / 1200) * kwota_pozyczki_poczatkowa - 200
+roznica = kwota_pozyczki_poczatkowa - pozostala_kwota_pozyczki
+podsumowanie_miesiaca = szablon.format(nr_miesiaca + 1, (round(pozostala_kwota_pozyczki, 2)), (round(roznica, 2)))
+print(podsumowanie_miesiaca)
